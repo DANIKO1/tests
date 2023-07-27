73861c3f070f02ed38c2a0d5a25af8ed51a8f24b
Daniel Koszerny <danielkoszerny@gmail.com>
2023-07-28 00:33:09 +0200

    Zadanie: żądania przy użyciu Postmana

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,34 +1,34 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/tasks")
 public class TaskController {
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks")
+
+    @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks/{taskId}")
-    public TaskDto getTask(Long taskId) {
+    @RequestMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
-    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
+    @RequestMapping
     public void deleteTask(Long taskId) {
 
     }
-    @RequestMapping(method = RequestMethod.PUT, value = "/tasks")
+    @RequestMapping(method = RequestMethod.PUT, value = "{taskId}")
     public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+        return new TaskDto(3L, "Edited test title", "Test content");
     }
-    @RequestMapping(method = RequestMethod.POST, value = "/tasks")
-    public void createTask(TaskDto taskDto) {
+    @RequestMapping(method = RequestMethod.POST, value = "{taskId}")
+    public TaskDto createTask(TaskDto taskDto) {
+        return new TaskDto(5L, "Edited test", "Test_content");
 
     }
 }
